5da346a4f3a9dddea926eddac90ad8c5bb64c2f3
wnw <ningweiwang218830@sohu-inc.com>
2022-06-02 16:14:45 +0800

    add: echarts

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './less/main.less'
 import logonComponents from './components/config'
+import * as echarts from 'echarts'
 
 Vue.config.productionTip = false
 
 logonComponents(Vue)
+
 Vue.use(ElementUI)
+Vue.use(echarts)
 
 new Vue({
   router,
