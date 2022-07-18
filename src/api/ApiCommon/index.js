41db6dd17497642efe60159584b64746669e3eb4
wnw <ningweiwang218830@sohu-inc.com>
2022-07-18 19:11:20 +0800

    update:增加pv上报

--- src/api/ApiCommon/index.js
+++ src/api/ApiCommon/index.js
@@ -2,6 +2,7 @@ import Http from '../../services/index'
 
 const apis = {
   getQiniuToken: (params) => Http.post(`http://data.anlan.xyz/upload`, params),
+  reportPV: (params) => Http.get(`http://data.anlan.xyz/reportPV`, params)
 }
 
 export default apis
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import apis from '@/api'
 
 Vue.use(VueRouter)
 
@@ -49,6 +50,36 @@ export default router
 router.beforeEach((to, from, next) => {
   // 若无特殊title则默认展示title为游戏中心
   window.document.title = to.meta.title || '游戏中心';
+  let spmC = ''
+  if (to.path === '/') {
+    spmC = 'home'
+  } else {
+    const baseSpmC = to.path.split('/')
+    if (baseSpmC[0] === '') {
+      baseSpmC.shift()
+    }
+    const end = baseSpmC[baseSpmC.length - 1]
+    var n = Number(end);
+    if (!isNaN(n) && n !== 404) {
+      baseSpmC.pop()
+    }
+    spmC = baseSpmC.join('-')
+  }
+  const year = new Date().getFullYear();
+  let month = new Date().getMonth() + 1;
+  let day = new Date().getDate();
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+  if (day < 10) {
+    day = "0" + day;
+  }
+  const params = {
+    setDate: `${year}/${month}/${day}`,
+    spm: `smpc.anlan-game.${spmC}`
+  }
+  apis.reportPV(params)
   // 获取即将进入的路由信息存入vuex
   let moduleType = to.fullPath.split('/')[1]
   store.commit('SetAsyncRouteReady', moduleType)
