2e61168613b4c6fbbb84338952ea7449c42c3a99
wnw <ningweiwang218830@sohu-inc.com>
2022-07-19 16:58:20 +0800

    init: login

--- src/api/ApiCommon/index.js
+++ src/api/ApiCommon/index.js
@@ -1,7 +1,9 @@
 import Http from '../../services/index'
 
 const apis = {
+  // 上传接口
   getQiniuToken: (params) => Http.post(`http://data.anlan.xyz/upload`, params),
+  // pv上报
   reportPV: (params) => Http.get(`http://data.anlan.xyz/reportPV`, params)
 }
 
---
+++ src/components/AlLogin.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <al-upload @imgSrc="getImgSrc"></al-upload>
+  </div>
+</template>
+
+<script>
+import apis from '@/api'
+export default {
+  name: 'AlLogin',
+  methods: {
+    register() {
+      const src = this.getImgSrc
+      const params = {
+
+      }
+      apis.register(params).then(res => {
+
+      })
+    },
+    login() {
+      const params = {
+
+      }
+      apis.login(params).then(res => {
+
+      })
+    },
+    getImgSrc(src) {
+      return src || ''
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
