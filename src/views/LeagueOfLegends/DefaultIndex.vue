4f0206a543d35a07e21119ea4d81d12e9de8d48b
luzongpeng <2985472046@qq.com>
2022-08-12 11:47:42 +0800

    删除500条数据逻辑

--- src/views/LeagueOfLegends/DefaultIndex.vue
+++ src/views/LeagueOfLegends/DefaultIndex.vue
@@ -8,21 +8,6 @@
     <el-button @click="showModuleList">切换</el-button>
     <div>英雄联盟主页</div>
     <el-button @click="$fn.login">登录</el-button>
-    <el-row v-for="(item, index) in arr" :key="index">
-      这是第{{ item.value }}条数据
-      <el-dropdown @visible-change="changeDrop">
-        <span class="el-dropdown-link">
-          <i class="el-icon-arrow-down el-icon--right"></i>
-        </span>
-        <el-dropdown-menu slot="dropdown">
-          <template v-if="dropdownShow">
-            <el-dropdown-item v-for="num in number" :key="num">
-              将此条数据移动到第{{ num }}条
-            </el-dropdown-item>
-          </template>
-        </el-dropdown-menu>
-      </el-dropdown>
-    </el-row>
   </section>
 </template>
 
@@ -34,15 +19,7 @@ export default {
       isShowModuleList: false, // 控制切换菜单是否展示，默认不展示
       number: 500,
       arr: [],
-      dropdownShow: false,
     };
-  },
-  mounted() {
-    for (let i = 0; i < this.number; i++) {
-      this.arr.push({
-        value: i,
-      });
-    }
   },
   methods: {
     // 展示切换菜单
@@ -52,9 +29,6 @@ export default {
     // 点击灰色遮罩关闭切换菜单
     closeModuleList() {
       this.isShowModuleList = false;
-    },
-    changeDrop() {
-      this.dropdownShow = true;
     },
   },
 };
