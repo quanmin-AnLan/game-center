82924af9c33f9a5b6f56e3f7598ea836ac4cf5a9
wnw <ningweiwang218830@sohu-inc.com>
2022-08-15 19:58:07 +0800

    update: 注释

--- src/api/ApiUser/index.js
+++ src/api/ApiUser/index.js
@@ -9,7 +9,7 @@ const apis = {
     register: (params) => Http.post(`${baseApi}/register`, params),
     // 用户列表
     getUserList: () => Http.get(`${baseApi}/getList`),
-    // 用户列表
+    // 用户编辑
     editUser: (params) => Http.post(`${baseApi}/edit`, params),
     // 更改权限
     changeUserAuth: (params) => Http.post(`${baseApi}/auth`, params),
