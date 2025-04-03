5d772ffa3721ac2136b99b3f4fa64e81ce598195
wnw <ningweiwang218830@sohu-inc.com>
2025-04-03 15:09:47 +0800

    update tft

--- src/views/Tools/children/TFTTools.vue
+++ src/views/Tools/children/TFTTools.vue
@@ -1,21 +1,32 @@
 <template>
-  <div v-loading="loading">
+  <div v-loading="loading" :element-loading-text="loadingText">
     <div class="common-title">筛选区</div>
     <section class="search">
       <div class="search-item">
-        <div class="search-item-title">选择英雄种族：</div>
+        <div class="search-item-title">英雄种族：</div>
         <div class="operate-item">
           <el-select v-model="raceKey" placeholder="请选择英雄种族">
             <el-option v-for="(item, index) in raceSelectOptions" :key="index" :label="item.label"
               :value="item.value"></el-option>
           </el-select>
         </div>
-        <div class="search-item-title">选择英雄职业：</div>
+        <div class="search-item-title">英雄职业：</div>
         <div class="operate-item">
           <el-select v-model="jobKey" placeholder="请选择英雄职业">
             <el-option v-for="(item, index) in jobSelectOptions" :key="index" :label="item.label"
               :value="item.value"></el-option>
           </el-select>
+        </div>
+        <div class="search-item-title">英雄费用：</div>
+        <div class="operate-item">
+          <el-select v-model="priceKey" placeholder="请选择英雄费用">
+            <el-option v-for="(item, index) in priceSelectOptions" :key="index" :label="item.label"
+              :value="item.value"></el-option>
+          </el-select>
+        </div>
+        <div class="search-item-title">英雄名称：</div>
+        <div class="operate-item">
+          <el-input v-model="nameKey" placeholder="请输入英雄名称"></el-input>
         </div>
       </div>
     </section>
@@ -30,7 +41,7 @@
     </div>
     <div class="common-title">当前队伍</div>
     <div class="champion-show">
-      <div v-for="(item, index) in chooseData" :key="index" class="champion-item">
+      <div v-for="(item, index) in chooseData" :key="index" class="champion-item click" @click="addChampion(item)">
         <el-tooltip :content="item.displayName" placement="top">
           <el-image :src="imgStr + item.name" :alt="item.displayName" fit="fill" />
         </el-tooltip>
@@ -39,7 +50,7 @@
     <div class="common-title">当前羁绊</div>
     <div class="job-show">
       <div class="job-item" v-for="(item, index) in chooseRaceJobData" :key="index">
-        <div class="job-item-text">{{ item.name }}：</div>
+        <div class="job-item-text">{{ item.name }}{{ item.active ? `(${item.active})` : '' }}：</div>
         <div class="job-item-num">{{ item.num + '/' + item.level }}</div>
       </div>
     </div>
@@ -48,10 +59,10 @@
       <div class="ai-operate">
         <div class="ai-operate-item">目标队伍人数：</div>
         <el-input-number v-model="aiNum" :max="10" :min="1" class="ai-operate-item"></el-input-number>
-        <el-button @click="aiTeam">开始推演</el-button>
+        <el-button @click="aiTeam()" :disabled="loading">{{ aiText }}</el-button>
       </div>
     </div>
-    <div class="common-title">推演队伍</div>
+    <div class="common-title">推演队伍{{ aiChampionData.length ? `(生成了${aiChampionData.length}个队伍)` : '' }}</div>
     <div style="width: 1200px; margin: 0 auto;">
       <div v-for="(data, i) in aiChampionData" :key="i">
         <div style="width: 100%; display: flex; flex-wrap: wrap;">
@@ -63,7 +74,7 @@
         </div>
         <div style="width: 100%; display: flex; flex-wrap: wrap;">
           <div class="job-item" v-for="(item, index) in aiChampionJobData[i]" :key="index">
-            <div class="job-item-text">{{ item.name }}：</div>
+            <div class="job-item-text">{{ item.name }}{{ item.active ? `(${item.active})` : '' }}：</div>
             <div class="job-item-num">{{ item.num + '/' + item.level }}</div>
           </div>
         </div>
@@ -85,11 +96,16 @@ export default {
       jobData: [],
       raceSelectOptions: [{label: '全部', value: ''}],
       jobSelectOptions: [{ label: '全部', value: '' }],
+      priceSelectOptions: [{ label: '全部', value: '' }],
       raceKey: '',
       jobKey: '',
+      priceKey: '',
+      nameKey: '',
       chooseData: [],
       aiNum: 10,
-      aiChampionData: []
+      aiChampionData: [],
+      aiText: '开始推演',
+      loadingText: '拼命加载中'
     }
   },
   computed: {
@@ -100,6 +116,12 @@ export default {
       }
       if (this.raceKey) {
         result = result.filter(item => item.races.includes(this.raceKey))
+      }
+      if (this.priceKey) {
+        result = result.filter(item => Number(item.price) === this.priceKey)
+      }
+      if (this.nameKey) {
+        result = result.filter(item => item.displayName.includes(this.nameKey))
       }
       return result
     },
@@ -116,17 +138,19 @@ export default {
           if (resultJobItem) {
             resultJobItem.num++
             for (const j in findItem.level) {
-              if (resultJobItem.num >= findItem.level[j]) {
-                if (findItem.level[Number(j + 1)]) {
-                  resultJobItem.level = findItem.level[Number(j + 1)]
+              if (resultJobItem.num >= Number(findItem.level[Number(j)])) {
+                if (findItem.level[Number(j) + 1]) {
+                  resultJobItem.level = findItem.level[Number(j) + 1]
                 }
+                resultJobItem.active = findItem.level[Number(j)]
               }
             }
           } else {
             result.push({
               name: i,
               num: 1,
-              level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1)
+              level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1),
+              active: findItem.level[0] !== '1' ? 0 : 1
             })
           }
         }
@@ -148,17 +172,19 @@ export default {
             if (resultJobItem) {
               resultJobItem.num++
               for (const j in findItem.level) {
-                if (resultJobItem.num >= findItem.level[j]) {
-                  if (findItem.level[Number(j + 1)]) {
-                    resultJobItem.level = findItem.level[Number(j + 1)]
+                if (resultJobItem.num >= Number(findItem.level[Number(j)])) {
+                  if (findItem.level[Number(j) + 1]) {
+                    resultJobItem.level = findItem.level[Number(j) + 1]
                   }
+                  resultJobItem.active = findItem.level[Number(j)]
                 }
               }
             } else {
               res.push({
                 name: i,
                 num: 1,
-                level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1)
+                level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1),
+                active: findItem.level[0] !== '1' ? 0 : 1
               })
             }
           }
@@ -212,6 +238,12 @@ export default {
             label: item.name
           })
         }
+        for (let i = 1; i < 6; i++) {
+          this.priceSelectOptions.push({
+            value: i,
+            label: i
+          })
+        }
         this.loading = false
       } catch (err) {
         this.loading = false
@@ -226,7 +258,14 @@ export default {
         this.chooseData.splice(index, 1)
       }
     },
-    aiTeam() {
+    sleep (time) {
+      return new Promise(res => {
+        setTimeout(res, time)
+      })
+    },
+    async aiTeam() {
+      this.loading = true
+      this.aiText = '推演中'
       if (this.aiNum < this.chooseData.length) {
         this.$message.error('目标人数小于已选队伍人数')
       }
@@ -242,21 +281,52 @@ export default {
         for (const index in result.data) {
           const obj = {
             data: result.data[index],
-            job: result.job[index]
+            job: result.job[index],
+            idx: Number(index),
+            total: result.data.length
           }
-          const { data, job } = this.loop(obj)
+          const { data, job } = await this.loop(obj)
           oneResult.data.push(...data)
           oneResult.job.push(...job)
+          await this.sleep(100)
         }
         result = oneResult
       }
-      this.aiChampionData = result.data
+      // 使用缓存优化性能
+      const cache = new Map();
+
+      const getIdentifier = (subArr) => {
+        const key = subArr.map(obj =>
+          JSON.stringify(
+            Object.keys(obj).sort().reduce((acc, key) => {
+              acc[key] = obj[key];
+              return acc;
+            }, {})
+          )
+        ).sort().join('|');
+
+        if (!cache.has(key)) {
+          cache.set(key, true);
+          return key;
+        }
+        return null;
+      };
+
+      this.aiChampionData = result.data.reduce((acc, subArr) => {
+        const id = getIdentifier(subArr);
+        if (id) acc.push(subArr);
+        return acc;
+      }, []);
+      this.loading = false
+      this.aiText = '开始推演'
     },
-    loop(tempData) {
+    async loop(tempData) {
       const data = this.championData.filter((v) => tempData.data.every((val) => val.name != v.name))
       const newData = []
       const newJobs = []
+      let index = 1
       for (const item of data) {
+        this.loadingText = `${tempData.data.length}人口有${tempData.total}队，正在推演基于第${tempData.idx + 1}队的${tempData.data.length + 1}人口的第${index}种可能性`
         const jobArr = item.jobs && item.jobs.split(',') || []
         const raceArr = item.races && item.races.split(',') || []
         const arr = jobArr.concat(raceArr)
@@ -269,21 +339,27 @@ export default {
           if (resultJobItem) {
             resultJobItem.num++
             for (const j in findItem.level) {
-              if (resultJobItem.num >= findItem.level[j]) {
-                if (findItem.level[Number(j + 1)]) {
+              if (resultJobItem.num >= Number(findItem.level[Number(j)])) {
+                if (findItem.level[Number(j) + 1]) {
                   // 晋级1个羁绊
-                  if (findItem.level[Number(j + 1)] > resultJobItem.level) {
+                  if (findItem.level[Number(j) + 1] > resultJobItem.level) {
                     tempResult = true
                   }
-                  resultJobItem.level = findItem.level[Number(j + 1)]
+                  resultJobItem.level = findItem.level[Number(j) + 1]
                 }
+                resultJobItem.active = findItem.level[Number(j)]
+                await this.sleep(1)
               }
             }
           } else {
+            if (findItem.level[0] === '1' && findItem.level[1]) {
+              tempResult = true
+            }
             tempJobs.push({
               name: i,
               num: 1,
-              level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1)
+              level: findItem.level[0] !== '1' ? findItem.level[0] : (findItem.level[1] ? findItem.level[1] : 1),
+              active: findItem.level[0] !== '1' ? 0 : 1
             })
           }
         }
@@ -292,6 +368,7 @@ export default {
           baseData.push(item)
           newData.push(baseData)
           newJobs.push(tempJobs)
+          index++
         }
       }
       return { data: newData, job: newJobs }
@@ -317,7 +394,7 @@ export default {
     margin-bottom: 8px;
 
     .operate-item {
-      margin: 0 24px;
+      margin-right: 8px;
     }
   }
 }
